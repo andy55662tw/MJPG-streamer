1b7249f0a520a565a06a8fe5e047d67ce7f1b68d
PeterHsu <andy55662tw@gmail.com>
2019-09-29 09:37:22 +0800

    v1.1

--- mjpgstream_picam_1.sh
+++ mjpgstream_picam_1.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install subversion libjpeg8-dev imagemagick libv4l-dev -y
+cd ~
 svn co https://svn.code.sf.net/p/mjpg-streamer/code/
 cd /home/pi/code/mjpg-streamer
 make
@@ -10,12 +11,12 @@ sudo make install
 cd ~
 wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc && sudo apt-key add ./lrkey.asc
 sudo chmod 777 /etc/apt/sources.list
-echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/source.list
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/sources.list
 sudo chmod 644 /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install uv4l uv4l-raspicam -y
 while true; do
-    read -p "Do you want to reboot? [Yes or No]:" yn
+    read -p "Do you want to reboot? [Y/n]:" yn
     case $yn in
         [Yy]* ) echo "Reboot after 3 seconds...";sleep 3s; sudo reboot;;
         [Nn]* ) echo "After rebooting, remember to execute the program \"mjpgstream_picam_2\"";exit;;
--- mjpgstream_picam_2.sh
+++ mjpgstream_picam_2.sh
@@ -6,7 +6,7 @@ sudo apt-get install uv4l-uvc -y
 sudo apt-get install uv4l-xscreen -y
 sudo apt-get install uv4l-mjpegstream -y
 while true; do
-    read -p "Do you want to reboot? [Yes or No]:" yn
+    read -p "Do you want to reboot? [Y/n]:" yn
     case $yn in
         [Yy]* ) echo "Reboot after 3 seconds...";sleep 3s; sudo reboot;;
         [Nn]* ) echo "After rebooting, remember to execute the program \"mjpgstream_picam_3\"";exit;;
